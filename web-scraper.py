df73e03d916c33f3133a1fd68d6e5ee1469538ec
Jordan Larot MM <jordanbiz@aliasville.com>
2022-08-14 21:08:46 -0700

    nothing really
    w

--- web-scraper.py
+++ web-scraper.py
@@ -1,15 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://transparentcalifornia.com/salaries/search/?q=superintendent&y=2020&page=1"
+# url = "https://transparentcalifornia.com/salaries/search/?q=superintendent&y=2020&page=1"
+# r = requests.get(url)
+# html_doc = r.text
+
+# soup = BeautifulSoup(html_doc)
+
+# #pretty_soup = soup.prettify()
+
+# names = soup.find_all('a')
+
+# for name in names:
+#     print(name.string)
+
+url = 'https://www.california-demographics.com/cities_by_population'
 r = requests.get(url)
 html_doc = r.text
 
 soup = BeautifulSoup(html_doc)
 
-#pretty_soup = soup.prettify()
-
-names = soup.find_all('a')
-
-for name in names:
-    print(name.string)
+print(soup.find_all('a'))
